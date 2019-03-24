25b03fb1ecdc83c293453775b3618c1818baabc5
Ahmed Yakout <iyakout@hotmail.com>
2019-03-24 21:45:34 +0200

    Some code in server

--- src/main/java/bbs/Start.java
+++ src/main/java/bbs/Start.java
@@ -2,7 +2,7 @@ package bbs;
 
 import bbs.utils.Configuration;
 
-importg java.io.FileInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
